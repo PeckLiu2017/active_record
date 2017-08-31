2826d2fb81d25e282ec57af2af6125f6cb67f2a6
peck liu <xiaoyang20170204@gmail.com>
2017-08-31 20:46:28 +0800

    study exclusion

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,7 @@ class Account < ApplicationRecord
   belongs_to :supplier
   has_one :accounthistroy
   belongs_to :representative
+  # the word "in" in next line  can be repalced by "with"
+  validates :subdomain, exclusion: { in: %w(www us ca jp),
+  message: "this value is reserved." }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170831112927) do
+ActiveRecord::Schema.define(version: 20170831123921) do
 
   create_table "accounthistroys", force: :cascade do |t|
     t.string   "order"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170831112927) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.integer  "representative_id"
+    t.string   "subdomain"
     t.index ["supplier_id"], name: "index_accounts_on_supplier_id", unique: true
   end
 
