1ab16b35a306cde68fe10654709ccc345fa78435
peck liu <xiaoyang20170204@gmail.com>
2017-09-01 23:33:21 +0800

    study content_type layout method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,6 +25,7 @@ class ProductsController < ApplicationController
     # render inline: "<% @products.each do |p| %><p><%= p.name %></p><% end %>"
     # render inline: "xml.p {'Horrid coding practice!'}", type: :builder
     # render plain: "OK"
+    # render body: "#{@products}"
   end
 
   def show
@@ -32,6 +33,9 @@ class ProductsController < ApplicationController
     # render html: "<strong>Not Found Test</strong>".html_safe
     # render json: @product
     # render xml: @product # test failed
+    # This XML file does not appear to have any style information associated with it. The document tree is shown below.
+    # render file: "/Users/peckliu/Ruby project/active_record/app/views/products/show.html.erb", content_type: "application/xml"
+    render layout: "special_layout"
   end
 
   private
