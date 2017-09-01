c64e1deedcb18503e2910d1abbdbb90d8318da77
peck liu <xiaoyang20170204@gmail.com>
2017-09-01 22:48:37 +0800

    study render html method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,6 +23,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    render html: "<strong>Not Found Test</strong>".html_safe
   end
 
   private
