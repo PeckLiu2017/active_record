beaa05b76067648837f64e36defc9452192a2bea
peck liu <xiaoyang20170204@gmail.com>
2017-09-06 14:11:15 +0800

    Sending Email To Multiple Recipients

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,15 @@
 class UserMailer < ApplicationMailer
-  default from: "from@example.com"
+  default to: Proc.new { ['1111111@qq.com','22222222@qq.com'] },
+    from: "from@example.com"
   layout 'mailer'
 
   def welcome_email(user)
     @user = user
-    email_with_name = %("#{@user.nickname}" <#{@user.email}>)
+    # email_with_name = %("#{@user.nickname}" <#{@user.email}>)
     @url = 'http://localhost:3000'
     # attachments = {}
     # attachments.inline['image.jpg'] = File.read('public/image.jpg')
-    mail(to: email_with_name, subject: 'Welcome to My Awesome Site')
+    # mail(to: email_with_name, subject: 'Welcome to My Awesome Site')
+    mail(subject: 'Welcome to My Awesome Site')
   end
 end
