6a4b77265a92cd76db1bc573dce382292b3eeb14
peck liu <xiaoyang20170204@gmail.com>
2017-09-09 17:39:25 +0800

    use route concern

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  concern :order_resources do
+    resources :orders
+  end
   resource :profile
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :people, :clients, :books, :articles
-  resources :clients do
-    resources :orders
-  end
+  # resources :clients do
+  #   resources :orders
+  # end
+
+  resources :clients, concerns: :order_resources
 
   resources :addresses
   resources :products do
